ecb4a8e73a6691b220a37db93a9e6f3b52f41c16
JHLowenstein <jhlGitHub@gmail.com>
2023-07-11 17:56:16 -0400

    adding hair product page

--- app/hair/components/hero.tsx
+++ app/hair/components/hero.tsx
@@ -83,7 +83,7 @@ export default function Hero() {
                 <a
                   className='btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4
 sm:w-auto sm:mb-0'
-                  href='#0'
+                  href='/hair/products'
                 >
                   Products
                 </a>
--- app/hair/components/ui/header.tsx
+++ app/hair/components/ui/header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
               </li>
               <li>
                 <Link
-                  href='/signin'
+                  href='/hair/products'
                   className='font-medium text-zinc-500 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out'
                 >
                   Products
